202f154928648cde38d6825a18b184a9c0bd95a4
Perry <davidlh257@gmail.com>
2024-10-30 10:33:23 -0600

    Cambios en comentarios y estructura

--- components/features/auth/infraestructure/datasources/authDatasoruceImp.ts
+++ components/features/auth/infraestructure/datasources/authDatasoruceImp.ts
@@ -35,12 +35,12 @@ class AuthDatasoruceImp implements Authsource{
 
     async register(name: string, lastName: string, password: string, phone: string, email: string): Promise<any> {
         try {
-            console.log("Datos de registro:", { name, lastName, password, phone, email }); // Para verificar los datos
+            console.log("Datos de registro:", { name, lastName, password, phone, email });
             const response = await apiClient.post('auth/register', { name, email, password, lastName, phone });
-            console.log("Respuesta del registro:", response); // Verificar la respuesta
+            console.log("Respuesta del registro:", response);
             return response.data;
         } catch (error) {
-            console.error("Error en registro:", error); // Verificar el error
+            console.error("Error en registro:", error);
             if (axios.isAxiosError(error)) {
                 throw new Error(error.response?.data?.message || 'Fallo en el registro');
             } else if (error instanceof Error) {
